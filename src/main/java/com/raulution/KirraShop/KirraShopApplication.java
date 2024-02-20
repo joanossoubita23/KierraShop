dcf227afd1499bf443972e43566a0a225710206f
joanossoubita23 <joanossoubita@yahoo.fr>
2024-02-20 17:45:48 -0500

    fisrt

---
+++ src/main/java/com/raulution/KirraShop/KirraShopApplication.java
@@ -0,0 +1,13 @@
+package com.raulution.KirraShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KirraShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KirraShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raulution/KirraShop/KirraShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raulution.KirraShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KirraShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
